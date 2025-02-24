cca89deeb1a66df3468b0d7d31ac624ff230091a
Jayana10 <jayana10302@gmail.com>
2025-02-24 22:40:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let btn= document.getElementById("colorSelect");
+btn.removeListener("click" => {
+	 var selectedIndex = select.selectedIndex;
+    
+    if (selectedIndex !== -1) {
+        select.remove(selectedIndex);
+});
