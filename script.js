910bccaca2fa17948a03b5dfe93359d0fad19f14
Jayana10 <jayana10302@gmail.com>
2025-02-28 21:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 //your JS code here. If required.
-let btn= document.getElementById("colorSelect");
-let select = document.getElementById("colorSelect");
-btn.addEventListener("click" => {
-	 var selectedIndex = select.selectedIndex;
+// let btn= document.getElementById("colorSelect");
+// let select = document.getElementById("colorSelect");
+// btn.addEventListener("click" => {
+// 	 var selectedIndex = select.selectedIndex;
     
+//     if (selectedIndex !== -1) {
+//         select.remove(selectedIndex);
+// });
+let btn = document.querySelector('input[type="button"]'); 
+let select = document.getElementById("colorSelect"); 
+
+btn.addEventListener("click", () => {
+    let selectedIndex = select.selectedIndex;
+
     if (selectedIndex !== -1) {
         select.remove(selectedIndex);
+    }
 });
